fec72e33d154b9c66af25b823972a79aecbf62bf
Luciolama <luciolattanzi1997@gmail.com>
2023-10-30 15:03:24 -0300

    persisten los datos en la pagina

--- frontend/src/stores/taskStore.js
+++ frontend/src/stores/taskStore.js
@@ -4,7 +4,7 @@ import { defineStore } from "pinia";
 export const useTaskStore = defineStore("taskStore", {
   state: () => ({
     tasks: [],
-    loading : false
+    loading: false,
   }),
   getters: {
     favourites() {
@@ -23,23 +23,51 @@ export const useTaskStore = defineStore("taskStore", {
     async getTasks() {
       this.loading = true;
 
-      const res = await fetch("http://localhost:3000/tasks");
+      const res = await fetch("http://localhost:3000/tasks"); //  se utiliza en JavaScript para realizar solicitudes de recursos en la web, como datos JSON, archivos, imágenes u otros recursos, desde un servidor o una ubicación en línea.
       const data = await res.json();
 
       this.tasks = data;
       this.loading = false;
     },
-    addTask(task) {
+    async addTask(task) {
       this.tasks.push(task);
+
+      const res = await fetch("http://localhost:3000/tasks", {
+        method: "POST",
+        body: JSON.stringify(task),
+        headers: { "Content-Type": "application/json" },
+      });
+
+      if (res.error) {
+        console.log(res.error);
+      }
     },
-    deleteTask(id) {
+    async deleteTask(id) {
       this.tasks = this.tasks.filter((task) => {
         return task.id !== id;
       });
+
+      const res = await fetch("http://localhost:3000/tasks/" + id, {
+        method: "delete",
+      });
+
+      if (res.error) {
+        console.log(res.error);
+      }
     },
-    toggleFav(id) {
+    async toggleFav(id) {
       const task = this.tasks.find((task) => task.id === id);
       task.isFav = !task.isFav;
-    },
-  },
+
+      const res = await fetch("http://localhost:3000/tasks/"+id, {
+        method: "PATCH",
+        body: JSON.stringify({isFav : task.isFav}),
+        headers: { "Content-Type": "application/json" }
+      })
+
+      if (res.error) {
+        console.log(res.error);
+      }
+    }
+  }
 });
